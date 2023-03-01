7dbc749c94280fdddb8c57487b03743d4ff4059d
Divyaansh16 <97334111+Divyaansh16@users.noreply.github.com>
2023-03-01 17:54:09 +0530

    Add files via upload

---
+++ DownloadAndMove.py
@@ -0,0 +1,54 @@
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from_dir = "C:/Users/Secret user/Downloads"
+to_dir = "/Scrap/C-115 Scrap"
+dir_tree={
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.xlsx' '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+class FileMovementHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        name, extension = os.path.splitext(event.src_path)
+        time.sleep(1)
+        for key, value in dir_tree.items():
+            time.sleep(1)
+            if extension in value:
+                file_name = os.path.basename(event.src_path)
+                print("Downloaded " + file_name)
+                path1 = from_dir + '/' + file_name
+                path2 = to_dir + '/' + key
+                path3 = to_dir + '/' + key + '/' + file_name
+                if os.path.exists(path2):
+                    print("Directory Exists...")
+                    if os.path.exists(path3):
+                        print("File alredy exists")
+                        new_file_name=os.path.splitext(file_name)[0]+str(random.randint(0,100))+os.path.splitext(file_name)[1]
+                        path4=to_dir+'/'+new_file_name
+                        shutil.move(path1,path4)
+                    else:
+                        print("Moving " + file_name + "....")
+                        shutil.move(path1, path3)
+                        time.sleep(1)
+                else:
+                    print("Making Directory...")
+                    os.makedirs(path2)
+                    print("Moving " + file_name + "....")
+                    shutil.move(path1, path3)
+                    time.sleep(1)
+event_handler = FileMovementHandler()
+observer = Observer()
+observer.schedule(event_handler, from_dir, recursive=True)
+observer.start()
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    print("stopped!")
+    observer.stop()
